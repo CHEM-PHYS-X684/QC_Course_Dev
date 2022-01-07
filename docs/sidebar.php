dca970725c52668fe2a9617dc9f4e0dbff87891f
Nick Mayhall <nmayhall@vt.edu>
2022-01-07 16:26:21 -0500

    added docs

---
+++ docs/sidebar.php
@@ -0,0 +1,48 @@
+
+<!-- Sidebar -->
+<div id="sidebar">
+	<div class="inner">
+
+		<!-- Search -->
+		<section id="search" class="alt">
+			<form method="post" action="#">
+				<input type="text" name="query" id="query" placeholder="Search" />
+			</form>
+		</section>
+
+		<!-- Menu -->
+		<nav id="menu">
+			<header class="major">
+				<h2>Menu</h2>
+			</header>
+			<ul>
+				<li><a href="index.html">Homepage</a></li>
+				<li><a href="generic.html">Syllabus</a></li>
+				<li>
+					<span class="opener">Modules</span>
+					<ul>
+						<li><a href="#">Module 1</a></li>
+						<li><a href="#">Module 2</a></li>
+						<li><a href="#">Module 3</a></li>
+						<li><a href="#">Module 4</a></li>
+						<li><a href="#">Module 5</a></li>
+					</ul>
+				</li>
+				<li>
+					<span class="opener">Math Review</span>
+					<ul>
+						<li><a href="#">Linear Algebra</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+
+
+		<!-- Footer -->
+		<footer id="footer">
+			<p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+		</footer>
+
+	</div>
+</div>
+
